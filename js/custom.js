466bf98a75084719a24c36da45cd1967d20d94ca
SS11146111 <sahasangita59@gmail.com>
2024-09-06 21:41:40 +0530

    completed phase 1

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,16 @@ function register()
 {
     alert("Registration Successful!!")
 }
+window.onload = function() {
+    // Show the spinner initially
+    document.querySelector('.spinLoad').style.display = 'block';
+    document.querySelector('.container').style.display = 'none';
+    // Hide the spinner after 3 seconds
+    setTimeout(function() {
+      document.querySelector('.spinLoad').style.display = 'none';
+      document.querySelector('.container').style.display= 'block';
+    }, 1000);
+
+    
+}
+
